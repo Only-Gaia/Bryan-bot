ccc30f782979b309a65ce09757ca52958b7e9b2d
Only-Gaia <spinello.gaia@scuolaninomartoglio.edu.it>
2026-09-11 11:13:28 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# ================= BOT =================
+BOT_TOKEN = os.getenv("BOT_TOKEN", "INSERISCI_QUI_IL_TUO_TOKEN")
+PREFIX = ";"  # prefisso per i comandi testuali (in aggiunta agli slash "/")
+OWNER_IDS = []  # ID Discord dei proprietari/admin principali del bot
+EMBED_COLOR = 0xFFA500
